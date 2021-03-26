b88000cd6ee4ff965ec2a321d24573d30e7c8e62
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-03-26 13:52:13 +0300

    Add class quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{Quote} from "./quote"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'The way to get started is to quit talking and begin doing.';
+  quote:Quote[] = [
+    {id:1, quote:'Watch finding Nemo'},
+    {id:2,quote:'Buy Cookies'},
+    {id:3,quote:'Get new Phone Case'},
+    {id:4,quote:'Get Dog Food'},
+    {id:5,quote:'Solve math homework'},
+    {id:6,quote:'Plot my world domination plan'},
+  ];
 }
