3cecb76022d4d4d13ee01be3d28aaa6d6bdc288c
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-03-26 13:42:40 +0300

    Ad quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'The way to get started is to quit talking and begin doing.';
 }
